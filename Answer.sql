152c732c0a38a80e3766230a3fcadb126afb7c71
Farah-salat <salatfarah219@gmail.com>
2026-09-21 09:23:35 +0300

    Add SQL queries for payment and customer analysis
    Advanced SQL Queries and Aggregations

---
+++ Answer.sql
@@ -0,0 +1,38 @@
+-- Question 1
+-- Total payment amount for each payment date
+SELECT
+    paymentDate,
+    SUM(amount) AS total_amount_paid
+FROM payments
+GROUP BY paymentDate
+ORDER BY paymentDate DESC
+LIMIT 5;
+
+
+-- Question 2
+-- Average credit limit of each customer
+SELECT
+    customerName,
+    country,
+    AVG(creditLimit) AS average_credit_limit
+FROM customers
+GROUP BY customerName, country;
+
+
+-- Question 3
+-- Total price of products ordered
+SELECT
+    productCode,
+    quantityOrdered,
+    SUM(quantityOrdered * priceEach) AS total_price
+FROM orderdetails
+GROUP BY productCode, quantityOrdered;
+
+
+-- Question 4
+-- Highest payment amount for each check number
+SELECT
+    checkNumber,
+    MAX(amount) AS highest_amount_paid
+FROM payments
+GROUP BY checkNumber;
